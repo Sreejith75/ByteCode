813e1dc2d119a5dbaa20c7d853e39ef93426419a
Sreejith K <sreejith.k@bytestrone.com>
2025-04-08 16:10:47 +0530

    Add placeholder class for Bytestrone.Core library

---
+++ ByteCode.DocumentProcessor/Placeholder.cs
@@ -0,0 +1,10 @@
+﻿namespace ByteCode.DocumentProcessor;
+
+public class DocumentProcessor;
+/// <summary>
+/// Reserved placeholder for Bytestrone.Core library.
+/// </summary>
+public static class Placeholder
+{
+    public const string Version = "0.0.1-alpha";
+}
